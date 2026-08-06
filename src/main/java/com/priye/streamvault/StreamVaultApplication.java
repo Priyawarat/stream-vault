143c7c6aaad55d877906c83f6a6798fe4e2c2c5a
Priyawarat <priyearya223@gmail.com>
2026-08-06 22:47:18 +0530

    Initial commit

---
+++ src/main/java/com/priye/streamvault/StreamVaultApplication.java
@@ -0,0 +1,13 @@
+package com.priye.streamvault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamVaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamVaultApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/priye/streamvault/StreamVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.priye.streamvault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamVaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
